2ae94c14efa7d5d901e94dd138179f55fc27ea6f
Emily Dror <emilydror@Emilys-MacBook-Air.local>
2023-05-07 16:32:42 +0300

    take 1

--- ex1.asm
+++ ex1.asm
@@ -2,4 +2,23 @@
 
 .section .text
 _start:
-#your code here
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $16, %rsp
+    movq %rcx, -8(%rbp)            # saving rcx
+    movq %rax, -16(%rbp)            # saving rax
+    movq $0, %rcx
+    movb $0, Bool
+    jmp _L2_ex1
+_L1_ex1:
+    movq num, %rax
+    shrq %cl, %rax
+    andq $1, %rax              # lsb
+    addb %al, Bool            # Bool += num[i]
+    addq $1, %rcx                   # ++i
+_L2_ex1:
+    cmpq $64, %rcx 
+    jl _L1_ex1                      # if i < 64 GOTO L1_ex1
+    movq -8(%rbp), %rcx             # recovering rcx
+    movq -16(%rbp), %rax            # recovering rax
+    leave 
