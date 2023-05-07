fbe3e97cfeb40146fe531bd7cb80aaed641c585e
Emily Dror <emilydror@Emilys-MacBook-Air.local>
2023-05-07 21:04:43 +0300

    take 1

--- ex3.asm
+++ ex3.asm
@@ -2,4 +2,25 @@
 
 .section .text
 _start:
-#your code here
+    xor r1, r1
+    xor r2, r2
+    xor r3, r3
+    jmp L1
+L4:
+    movl array1(,r1, 4), eax
+    cmpl array2(,r2, 4), eax
+    jge L2
+    movl eax, merged(,r3,4)
+    incq r1
+    jmp L3
+L2:
+    movl array2(,r2, 4), eax
+    movl eax, merged(,r3,4)
+    incq r2
+L3:
+    incq r3
+L1:
+    cmpl array1(,r1, 4), 0
+    jne L4
+    cmpl array2(,r2, 4), 0
+    jne L4
