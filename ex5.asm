a3f7aa5e7f83838a8c896b3015e957f60b340015
Emily Dror <you@example.com>
2023-05-09 17:04:22 +0300

    update ex5

--- ex5.asm
+++ ex5.asm
@@ -2,4 +2,24 @@
 
 .section .text
 _start:
-#your code here
+    movq $root, %rax
+    cmpq $0, (%rax)
+    je set_node
+    movq (%rax), %rax
+compare_key:
+    movq (%rax), %rbx
+    cmpq %rbx, (new_node)
+    jl insert_left
+    je end    
+insert_right:
+    addq $8, %rax
+insert_left:
+    addq $8, %rax
+insert_node:
+    cmpq $0, (%rax)
+    je set_node
+    movq (%rax), %rax
+    jmp compare_key
+set_node:
+    movq $new_node, (%rax)
+end:
