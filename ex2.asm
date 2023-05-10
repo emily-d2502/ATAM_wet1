f2fb9ec4645e72a40f70d42f41c2f7be53be9878
Emily Dror <you@example.com>
2023-05-10 19:26:10 +0300

    update ex2

--- ex2.asm
+++ ex2.asm
@@ -1,37 +1,40 @@
 .global_start
 
-.section .text
+.section .text 
 _start:
-    movl $num, %eax
-    cdqe 
-    js Negitive
-    
+	movq source, %rsi
+	movq destination, %rdi
+	movl num, %eax
+	testl %eax, %eax 
+	jl Negitive 
+	jmp Positive 
+
 Positive:
-    mov $destination, %eax
-    sub $source, %eax
-    cmp $num, %eax
-    jb Backwards
-
-Forward:
-    mov $0, %eax
-Forward_loop:
-    movb source(%eax), %cl
-    movb %cl, destinantion(%eax)
-    inc %eax
-    cmp num, %eax
-    jne Forward_loop
-    jmp end
-
+	sub %rsi, %rdi
+	cmp num, %rdi
+	jb Backwards
+	jmp Forwards
+Forwards:
+	movl $0, %eax
+	jmp Forwards_loop
+Forwards_loop:
+	movb source(%eax), %cl
+	movb %cl, destination(%eax)
+	inc %eax
+	cmp num, %eax
+	jne Forwards_loop
+	jmp end
 Backwards:
-    mov num, %eax
+	movl num, %eax 
+	jmp Backwards_loop
 Backwards_loop:
-    movb source-1(%eax), %cl
-    movb %cl, destination-1(%eax)
-    dec %eax 
-    jnz Backwards_loop    
-
-Negitive:
-    mov $0, %eax
-    movl destination(%eax), $num
-
+	movb source-1(%eax), %cl
+	movb %cl, destination-1(%eax)
+	dec %eax
+	jge Backwards_loop
+	jmp end
+Negative:
+	movl num, %ecx
+	movl %ecx, destination 
+	jmp end
 end:
