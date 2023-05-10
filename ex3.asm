444a580d5aae67308a8a6fe0cd849bd555fa60f7
Emily <you@example.com>
2023-05-10 20:40:52 +0000

    final

--- ex3.asm
+++ ex3.asm
@@ -5,22 +5,25 @@ _start:
     xor %rax, %rax
     xor %rbx, %rbx
     xor %rcx, %rcx
-    jmp _L1_hw1
-_L4_hw1:
+    movq $mergedArray, %r8
+    jmp _L4_hw1
+_L1_hw1:
     movl array1(, %rax, 4), %edx
     cmpl array2(, %rbx, 4), %edx
-    jge _L2_hw1
-    movl %edx, mergedArray(, %rcx, 4)
+    jl _L2_hw1
     incq %rax
     jmp _L3_hw1
-_L2_ex3:
+_L2_hw1:
     movl array2(, %rbx, 4), %edx
-    movl %edx, mergedArray(, %rcx, 4)
     incq %rbx
 _L3_hw1:
+    cmpl %edx, -4(%r8, %rcx, 4)
+    je _L4_hw1
+    movl %edx, (%r8, %rcx, 4)
     incq %rcx
-_L1_hw1:
-    cmpl array1(, %rax, 4), $0
-    jne _L4_hw1
-    cmpl array2(, %rbx, 4), $0
-    jne _L4_hw1
+_L4_hw1:
+    cmpl $0, array1(, %rax, 4)
+    jne _L1_hw1
+    cmpl $0, array2(, %rbx, 4)
+    jne _L1_hw1
+    movl $0, (%r8, %rcx, 4)
--- ex5.asm
+++ ex5.asm
@@ -2,24 +2,24 @@
 
 .section .text
 _start:
-    cmpq $0, (root)
     movq $root, %rax
-    je set_node_hw1
-    movq (root), %rax
-    jmp compare_key_hw1
-insert_right_hw1:
-    addq 8, %rax
-insert_left_hw1:
-    addq 8, %rax
-insert_node_hw1:
     cmpq $0, (%rax)
-    jne compare_key_hw1
-set_node_hw1:
-    movq $new_node, (%rax)
-    jmp end_hw1
+    je set_node_hw1
+    movq (%rax), %rax
 compare_key_hw1:
     movq (%rax), %rbx
     cmpq %rbx, (new_node)
-    jg insert_right_hw1
     jl insert_left_hw1
+    je end_hw1
+insert_right_hw1:
+    addq $8, %rax
+insert_left_hw1:
+    addq $8, %rax
+insert_node_hw1:
+    cmpq $0, (%rax)
+    je set_node_hw1
+    movq (%rax), %rax
+    jmp compare_key_hw1
+set_node_hw1:
+    movq $new_node, (%rax)
 end_hw1:
