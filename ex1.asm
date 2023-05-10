0a72d10b9e14b4fa524c78e0bbc1d40a29d753ce
Emily Dror <you@example.com>
2023-05-10 22:56:21 +0300

    update ex1

--- ex1.asm
+++ ex1.asm
@@ -2,11 +2,6 @@
 
 .section .text
 _start:
-    pushq %rbp
-    movq %rsp, %rbp
-    subq $16, %rsp
-    movq %rcx, -8(%rbp)            # saving rcx
-    movq %rax, -16(%rbp)            # saving rax
     movq $0, %rcx
     movb $0, Bool
     jmp _L2_ex1
@@ -19,6 +14,3 @@ _L1_ex1:
 _L2_ex1:
     cmpq $64, %rcx 
     jl _L1_ex1                      # if i < 64 GOTO L1_ex1
-    movq -8(%rbp), %rcx             # recovering rcx
-    movq -16(%rbp), %rax            # recovering rax
-    leave 
